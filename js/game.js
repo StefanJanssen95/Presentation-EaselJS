af812eeec1b0f8f6ad87268ae41b5074e633b151
Stefan Janssen <info@stefanjanssen.eu>
2016-03-12 15:18:03 +0100

    Fixed sprite not showing bug
    Width and Height was to big.

--- js/game.js
+++ js/game.js
@@ -1,13 +1,11 @@
 function startGame() {
     var data = {
         "images": [imgPlayer],
-        "frames": {"regX": 0, "height": 96, "count": 10, "regY": 0, "width": 75},
+        "frames": {"regX": 0, "height": 60, "count": 10, "regY": 0, "width": 44},
         "animations": {"walk": [0, 9, "walk"]}
     };
     var sheet = new createjs.SpriteSheet(data);
     var shape = new createjs.Sprite(sheet,"walk");
-    shape.x = 200;
-    shape.y = 200;
 
     stage.addChild(shape);
     stage.update();
